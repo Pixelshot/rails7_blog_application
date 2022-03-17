ab9823f328a6560517ad6700c5468a394f424ac0
pixelshot <pixelshot8@gmail.com>
2022-03-17 21:09:31 +0800

    Init commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # First method of defining route
+  # get 'about', to: 'pages#about'
+
+  # Second method of defining
+  get 'pages/about' 
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # Uncomment code below to set any of the pages we have as our default page
+  root "pages#home"
+end
