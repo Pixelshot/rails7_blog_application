9ca546fe46ab0af77264af90f5147180e232555b
pixelshot <pixelshot8@gmail.com>
2022-03-19 18:44:06 +0800

    Add styling to links and simple seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+(1..10).each { |i| Post.create(title: "Post Number #{i}", body: "Some context for post ##{i}") }
